08f39ec4ddf027649005a0f29a7a876954b3a6d1
Guangxing Wang <87854190+xingronaldo@users.noreply.github.com>
2023-06-30 20:35:07 +0800

    Update trainval.py

--- trainval.py
+++ trainval.py
@@ -18,9 +18,9 @@ def setup_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = False
-    torch.backends.cudnn.benchmark = True
-    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.deterministic = False #!
+    torch.backends.cudnn.benchmark = True      #!
+    torch.backends.cudnn.enabled = True        #! for accelerate training 
 
 
 class Trainval(object):
